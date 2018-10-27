b0b45fa632bb7b61e455433e1d6497d22e39fa7d
prao3 <44528013+prao3@users.noreply.github.com>
2018-10-27 16:42:02 -0400

    Finding the url of the site and trimming it to the essentials

--- background.js
+++ background.js
@@ -1,12 +1,21 @@
  chrome.runtime.onInstalled.addListener(function() {
-   chrome.tabs.onActiveChanged.addListener(function() {
-     chrome.tabs.onHighlightChanged.addListener(function(){
+   chrome.tabs.onActivated.addListener(function() {
+     chrome.tabs.onHighlighted.addListener(function(){
        console.log("Switched tab");
      });
      console.log("Tab state changed");
      chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
-       var tab = tabs[0];
-       console.log(tab.url);
+       var tab = tabs[0].url;
+       var sb = "";
+       for(var i = tab.length - 1; i > 0; i--){
+         if(tab.charAt(i - 4) == '.' && tab.charAt(i - 3) == 'c'
+         && tab.charAt(i - 2) == 'o' && tab.charAt(i - 1) == 'm' && tab.charAt(i) == '/'){
+           for(var j = 0; j <= i; j++){
+             sb = sb + tab.charAt(j);
+           }
+         }
+       }
+       console.log(sb);
      });
    });
  });
