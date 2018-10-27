c40b9590964f736fb52c02d17ce351e3a24ba6e2
prao3 <44528013+prao3@users.noreply.github.com>
2018-10-27 17:16:49 -0400

    Cleanup

--- background.js
+++ background.js
@@ -4,16 +4,16 @@ chrome.runtime.onInstalled.addListener(function() {
       console.log("Tab highlighted");
     });
     console.log("Tab activated");
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
-      var tab = tabs[0].url;
-      var sb = "";
-      if(tab.search(".com/") != -1){
-        for(var j = 0; j < tab.search(".com/") + 5; j++){
-          sb = sb + tab.charAt(j);
+      chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+        var tab = tabs[0].url;
+        var sb = "";
+        if(tab.search(".com/") != -1){
+          for(var j = 0; j < tab.search(".com/") + 5; j++){
+            sb = sb + tab.charAt(j);
+            }
           }
-        }
-        console.log(sb);
-    });
+          console.log(sb);
+        });
     chrome.tabs.onUpdated.addListener(function() {
       console.log("Tab updated");
       chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
