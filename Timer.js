85cc5e8ea77d8285d2f09db4095acd8f23e44430
beasterbro <43251102+beasterbro@users.noreply.github.com>
2018-10-27 18:29:58 -0400

    Added stop functionality

--- Timer.js
+++ Timer.js
@@ -1,14 +1,21 @@
 var minutesLabel = document.getElementById("minutes");
 var secondsLabel = document.getElementById("seconds");
 var totalSeconds = 0;
+var go = true;
+
+function stop(){
+  go = false;
+}
+
+if(go){
 setInterval(setTime, 1000);
+}
 
 function setTime() {
   ++totalSeconds;
   secondsLabel.innerHTML = pad(totalSeconds % 60);
   minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
 }
-
 function pad(val) {
   var valString = val + "";
   if (valString.length < 2) {
