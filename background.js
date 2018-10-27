1457840661a758b6870b82c76a148cb63a5cb1ad
prao3 <44528013+prao3@users.noreply.github.com>
2018-10-27 14:28:27 -0400

    Trying to fix new tab

--- background.js
+++ background.js
@@ -2,9 +2,9 @@
     chrome.storage.sync.set({color: '#3aa757'}, function() {
       console.log("The color is green.");
     });
-	 chrome.windows.onCreated.addListener(function() {
-
-     console.log("A new tab");
-
-   });
+    chrome.windows.onCreated.addListener(function() {
+      chrome.storage.sync.set(function() {
+        console.log("A new tab");
+      });
+    });
  });
