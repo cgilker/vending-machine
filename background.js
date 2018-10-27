741901ba105a37099c2a4ffed38ad013bf980434
beasterbro <43251102+beasterbro@users.noreply.github.com>
2018-10-27 14:30:47 -0400

    the tabs update

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
     chrome.storage.sync.set({color: '#3aa757'}, function() {
       console.log("The color is green.");
     });
-    chrome.windows.onCreated.addListener(function() {
+    chrome.tabs.onCreated.addListener(function() {
       chrome.storage.sync.set(function() {
         console.log("A new tab");
       });
