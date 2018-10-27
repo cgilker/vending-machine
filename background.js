4d88f09d1416464f2d032ccf0d73c0d80e67c573
prao3 <44528013+prao3@users.noreply.github.com>
2018-10-27 17:38:23 -0400

    Stores URLs with no repeats

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 chrome.runtime.onInstalled.addListener(function() {
+  var urls = [];
   chrome.tabs.onActivated.addListener(function() {
     chrome.tabs.onHighlighted.addListener(function(){
       console.log("Tab highlighted");
@@ -7,13 +8,22 @@ chrome.runtime.onInstalled.addListener(function() {
       chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
         var tab = tabs[0].url;
         var sb = "";
+        var isContained = false;
         if(tab.search(".com/") != -1){
           for(var j = 0; j < tab.search(".com/") + 5; j++){
             sb = sb + tab.charAt(j);
             }
           }
+        for(var i = 0; i < urls.length; i++){
+          if(sb == urls[i]){
+            isContained = true;
+          }
+        }
+        if(!isContained){
+          urls.push(sb);
           console.log(sb);
-        });
+        }
+      });
     chrome.tabs.onUpdated.addListener(function() {
       console.log("Tab updated");
       chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
@@ -24,7 +34,15 @@ chrome.runtime.onInstalled.addListener(function() {
             sb = sb + tab.charAt(j);
             }
           }
+        for(var i = 0; i < urls.length; i++){
+          if(sb == urls[i]){
+            isContained = true;
+          }
+        }
+        if(!isContained){
+          urls.push(sb);
           console.log(sb);
+        }
       });
     });
   });
