0f0ef568b908371a11744d3f8d51b928685d3f20
beasterbro <43251102+beasterbro@users.noreply.github.com>
2018-10-27 19:39:57 -0400

    formating

--- Timers.js
+++ Timers.js
@@ -1,38 +1,38 @@
 class Timers{
-var minutesLabel = document.getElementById("minutes");
-var secondsLabel = document.getElementById("seconds");
-var totalSeconds = 0;
-var go = true;
+  var minutesLabel = document.getElementById("minutes");
+  var secondsLabel = document.getElementById("seconds");
+  var totalSeconds = 0;
+  var go = true;
 
-//a method to stop the timer
-function stop(){
-  go = false;
-}
-
-//a method to start the timer again
-function go(){
-  go = true;
-}
-
-//the reason the timer can stop
-if(go){
-setInterval(setTime, 1000);
-}
-
-//a method to continue the timeline
-function setTime() {
-  ++totalSeconds;
-  secondsLabel = pad(totalSeconds % 60);
-  minutesLabel = pad(parseInt(totalSeconds / 60));
-}
-
-//a method so that numbers count properly
-function pad(val) {
-  var valString = val + "";
-  if (valString.length < 2) {
-    return "0" + valString;
-  } else {
-    return valString;
+  //a method to stop the timer
+  function stop(){
+    go = false;
   }
-}
+
+  //a method to start the timer again
+  function go(){
+    go = true;
+  }
+
+  //the reason the timer can stop
+  if(go){
+  setInterval(setTime, 1000);
+  }
+
+  //a method to continue the timeline
+  function setTime() {
+    ++totalSeconds;
+    secondsLabel = pad(totalSeconds % 60);
+    minutesLabel = pad(parseInt(totalSeconds / 60));
+  }
+
+  //a method so that numbers count properly
+  function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+      return "0" + valString;
+    } else {
+      return valString;
+    }
+  }
 }
