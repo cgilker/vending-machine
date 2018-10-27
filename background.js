44324695427ef96404ae5902c737ee5ad2ca0399
prao3 <44528013+prao3@users.noreply.github.com>
2018-10-27 16:08:02 -0400

    Getting the url

--- background.js
+++ background.js
@@ -4,5 +4,9 @@
        console.log("Switched tab");
      });
      console.log("Tab state changed");
+     chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+       var tab = tabs[0];
+       console.log(tab.url);
+     });
    });
  });
