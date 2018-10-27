bfd5fad11479705ee588b6c6c14be331a72b4ff6
beasterbro <43251102+beasterbro@users.noreply.github.com>
2018-10-27 18:50:29 -0400

    Added some comments

--- Timer.js
+++ Timer.js
@@ -3,23 +3,29 @@ var secondsLabel = document.getElementById("seconds");
 var totalSeconds = 0;
 var go = true;
 
+//a method to stop the timer
 function stop(){
   go = false;
 }
 
+//a method to start the timer again
 function go(){
   go = true;
 }
 
+//the reason the timer can stop
 if(go){
 setInterval(setTime, 1000);
 }
 
+//a method to continue the timeline
 function setTime() {
   ++totalSeconds;
   secondsLabel.innerHTML = pad(totalSeconds % 60);
   minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
 }
+
+//a method so that numbers count properly
 function pad(val) {
   var valString = val + "";
   if (valString.length < 2) {
