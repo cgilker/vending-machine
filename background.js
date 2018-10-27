da4e7647f58b422323362870dc928df4ed6227dc
prao3 <44528013+prao3@users.noreply.github.com>
2018-10-27 15:04:40 -0400

    Tracking when changing from a tab or making or deleting a tab

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
  chrome.runtime.onInstalled.addListener(function() {
-    chrome.tabs.onActiveChanged.addListener(function(){
-      console.log("Changed tab");
-    });
+   chrome.tabs.onActiveChanged.addListener(function() {
+     chrome.tabs.onHighlightChanged.addListener(function(){
+       console.log("Switched tab");
+     });
+     console.log("Tab state changed");
+   });
  });
