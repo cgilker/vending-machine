27536ee67767e7b1e56b5d5331ae11deaa7d21eb
prao3 <44528013+prao3@users.noreply.github.com>
2018-10-27 22:28:45 -0400

    Revert "Still doesn't work"
    This reverts commit 2dea5e736e4db566ea8971c723189ba8aee1a02f.

--- popup.js
+++
@@ -1,9 +0,0 @@
-/*
-var windows = chrome.extension.getViews({type : "popup"});
-if(windows) {
-  var popup = windows[0];
-  */
-  var min = popup.document.getElementById("minutes");
-  var sec = popup.document.getElementById("seconds");
-  assignTime(sec,min);
-//}
